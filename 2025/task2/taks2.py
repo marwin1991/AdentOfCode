9065207b4b65c8f64c3499510349b54a6a2bd481
marwin1991 <peter.zmilczak@gmail.com>
2025-12-02 14:18:32 +0100

    Add validation for invalid numbers and count their occurrences

--- 2025/task2/taks2.py
+++ 2025/task2/taks2.py
@@ -4,9 +4,16 @@ def read_file():
     with open(FILE_PATH) as f:
         return f.readline()
 
+def is_invalid(number):
+    str_number = str(number)
+    firstpart, secondpart = str_number[:len(str_number)//2], str_number[len(str_number)//2:]
+    return firstpart == secondpart
+
 def main():
     line = read_file()
     ranges = line.split(",")
+    invalid_count = 0
+    invalid_sum = 0
     for r in ranges:
         print("-------------------")
         print(r)
@@ -16,8 +23,14 @@ def main():
         n2 = int(numbers[1])
 
         for i in range(n1, n2+1):
-            print(i)
+            if len(str(i)) % 2 == 0:
+                if is_invalid(i):
+                    invalid_count += 1
+                    invalid_sum += i
+                    print(f"{i} is invalid")
 
+    print(f"invalid count: {invalid_count}")
+    print(f"invalid sum: {invalid_sum}")
 
 if __name__ == '__main__':
     main()
