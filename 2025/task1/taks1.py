336305bcabdd1a56a4afd6de0e6e307f98f43279
marwin1991 <peter.zmilczak@gmail.com>
2025-12-02 12:29:09 +0100

    gold star 2

--- 2025/task1/taks1.py
+++ 2025/task1/taks1.py
@@ -10,26 +10,42 @@ def main():
     instructions = read_file()
 
     state = 50
-    time_pointing_zero = 0
+    times_pointing_zero = 0
+    times_passes_zero = 0
     for instruction in instructions:
+        print(f"-------------------{instruction}beginning state: {state}")
+        start_state = state
         if instruction.startswith('L'):
-           state -= int(instruction[1:].strip())
+            state -= int(instruction[1:].strip())
         else:
             state += int(instruction[1:].strip())
 
-        while state > 99:
+        while state > 100:
             state -= 100
+            times_passes_zero += 1
+            print(f"-----pass zero")
+
+        if state == 100:
+            state -= 100
+
+        while state <= -100:
+            state += 100
+            times_passes_zero += 1
+            print(f"-----pass zero")
 
         while state < 0:
             state += 100
+            if start_state != 0:
+                times_passes_zero += 1
+                print(f"-----pass zero")
 
         print(f"{instruction} --> {state}")
 
         if state == 0:
-            time_pointing_zero+=1
+            times_pointing_zero+=1
 
     print("=========================")
-    print(time_pointing_zero)
+    print(f"{times_pointing_zero} + {times_passes_zero} = {times_pointing_zero + times_passes_zero}")
 
 
 
