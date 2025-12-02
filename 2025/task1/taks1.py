15fc8faff73bb7ad6e34f3180c2868aed087acf5
marwin1991 <peter.zmilczak@gmail.com>
2025-12-02 12:10:20 +0100

    correct task1 for input greater than 100

--- 2025/task1/taks1.py
+++ 2025/task1/taks1.py
@@ -8,18 +8,19 @@ def read_file():
 
 def main():
     instructions = read_file()
-    print(len(instructions))
+
     state = 50
     time_pointing_zero = 0
     for instruction in instructions:
         if instruction.startswith('L'):
-           state -= int(instruction[1:])
+           state -= int(instruction[1:].strip())
         else:
-            state += int(instruction[1:])
+            state += int(instruction[1:].strip())
 
-        if state > 99:
+        while state > 99:
             state -= 100
-        if state < 0:
+
+        while state < 0:
             state += 100
 
         print(f"{instruction} --> {state}")
