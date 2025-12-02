f11a052af6d6ea5cc01e02985efa2e6f403b06c3
marwin1991 <peter.zmilczak@gmail.com>
2025-12-02 14:36:20 +0100

    second gold star for task2

--- 2025/task2/taks2.py
+++ 2025/task2/taks2.py
@@ -4,10 +4,38 @@ def read_file():
     with open(FILE_PATH) as f:
         return f.readline()
 
+def split(string, n):
+    out = []
+    for i in range(0, len(string), n):
+        out.append(string[i:i+n])
+    return out
+
+def is_sequence(str_number, prefix):
+    if len(str_number) == 1:
+        return False
+
+    if len(prefix) > len(str_number):
+        return False
+
+
+    chunks = split(str_number, len(prefix))
+
+    for c in chunks:
+        if c != prefix:
+            return False
+
+    return True
+
 def is_invalid(number):
     str_number = str(number)
-    firstpart, secondpart = str_number[:len(str_number)//2], str_number[len(str_number)//2:]
-    return firstpart == secondpart
+    length = len(str_number)
+
+    for i in range(1, max(length//2,1) + 1):
+        if is_sequence(str_number, str_number[:i]):
+            return True
+
+    return False
+
 
 def main():
     line = read_file()
@@ -23,11 +51,10 @@ def main():
         n2 = int(numbers[1])
 
         for i in range(n1, n2+1):
-            if len(str(i)) % 2 == 0:
-                if is_invalid(i):
-                    invalid_count += 1
-                    invalid_sum += i
-                    print(f"{i} is invalid")
+            if is_invalid(i):
+                invalid_count += 1
+                invalid_sum += i
+                print(f"{i} is invalid")
 
     print(f"invalid count: {invalid_count}")
     print(f"invalid sum: {invalid_sum}")
