e453a52c7de4b24d311a136899a836d74c7c9589
marwin1991 <peter.zmilczak@gmail.com>
2025-12-03 07:09:46 +0100

    task3 gold star

--- 2025/task3/taks3.py
+++ 2025/task3/taks3.py
@@ -4,16 +4,35 @@ def read_file():
     with open(FILE_PATH) as f:
         return f.readlines()
 
-def largest_joltage(bank: list):
-    biggest = 0
-    for battery1_index in range(len(bank)):
-        for battery2_index in range(battery1_index+1, len(bank)):
-            print(f"{bank[battery1_index]} + {bank[battery2_index]}")
-            value = int(bank[battery1_index] + bank[battery2_index])
-            if value > biggest:
-                biggest = value
+def largest_digit(bank: list, start_index, stop_index):
+    max_digit = 0
+    max_digit_index = 0
 
-    return biggest
+    for battery1_index in range(start_index, len(bank) - stop_index):
+        val = int(bank[battery1_index])
+        if val > max_digit:
+            max_digit = val
+            max_digit_index = battery1_index
+
+    if max_digit == 0:
+        raise Exception("no max digit found")
+
+    return max_digit, max_digit_index
+
+
+
+def largest_joltage(bank: list, digits_number: int = 12):
+    number = ""
+    start_index = 0
+    stop_index = digits_number - 1
+
+    for i in range(digits_number):
+        max_digit, max_digit_index = largest_digit(bank, start_index, stop_index)
+        number += str(max_digit)
+        start_index = max_digit_index + 1
+        stop_index -= 1
+
+    return number
 
 
 
@@ -23,11 +42,12 @@ def main():
     sum_of_batteries = 0
 
     for bank in banks:
+        print(bank)
         bank = list(bank.strip())
         largest = largest_joltage(bank)
-        sum_of_batteries += largest
+        sum_of_batteries += int(largest)
 
-    print(f"sum of batteries: {sum_of_batteries }")
+    print(f"sum of batteries: {sum_of_batteries }") # part 1 = 17330
 
 if __name__ == '__main__':
     main()
