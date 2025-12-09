a6c186e8238da0c5975d9e832b348ca3d5972306
marwin1991 <peter.zmilczak@gmail.com>
2025-12-09 06:50:51 +0100

    task 9 input parsing and grid creation

--- 2025/task9/taks9b.py
+++ 2025/task9/taks9b.py
@@ -1,4 +1,4 @@
-FILE_PATH = "input.txt"
+FILE_PATH = "input-test.txt"
 
 def read_file():
     with open(FILE_PATH) as f:
@@ -10,6 +10,26 @@ def rectangle_area(p1, p2):
     x1, y1 = p2
     return (abs(x1 - x) + 1) * (abs(y1 - y) + 1)
 
+def min_max(points):
+    xs = [p[0] for p in points]
+    ys = [p[1] for p in points]
+    return min(xs), max(xs), min(ys), max(ys)
+
+def make_table(points):
+    min_x, max_x, min_y, max_y = min_max(points)
+    min_x -= 1
+    min_y -= 1
+    max_x += 1
+    max_y += 1
+
+    grid = []
+    point_set = set(points)
+    for y in range(max_y, min_y - 1, -1):
+        row = ""
+        for x in range(min_x, max_x + 1):
+            row += "#" if (x, y) in point_set else "."
+        grid.append(list(row))
+    return grid
 
 def main():
     lines = read_file()
@@ -19,18 +39,11 @@ def main():
         xy = row.strip().split(",")
         coords_base.append((int(xy[0]), int(xy[1])))
 
-    coords = []
-    ares = []
+    grid = make_table(coords_base)
+    for row in grid:
+        print(row)
 
-    for i in range(len(coords_base)):
-        for j in range(len(coords_base)):
-            if i != j:
-                ares.append(rectangle_area(coords_base[i], coords_base[j]))
-
-
-    largest = max(ares)
-
-    print("Largest value:", largest)
+    print("Largest value:", 1)
 
 
 
