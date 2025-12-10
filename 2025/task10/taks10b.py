04f6821846d2425047c643d2172ea96c15f924d1
marwin1991 <peter.zmilczak@gmail.com>
2025-12-10 08:45:44 +0100

    init task 10

---
+++ 2025/task10/taks10b.py
@@ -0,0 +1,12 @@
+FILE_PATH = "input.txt"
+
+def read_file():
+    with open(FILE_PATH) as f:
+        return f.readlines()
+
+def main():
+    lines = read_file()
+
+
+if __name__ == '__main__':
+    main()
